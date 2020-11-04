7c2f7a51c6ec78fb424dd3414f4cbbf9b68adc05
Domenic Catalano <domenic.catalano@dcmail.ca>
2020-11-04 11:32:18 -0500

    implemented Person class in main

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,10 +1,11 @@
 package ca.durhamcollege;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-	// write your code here
-
-        System.out.println("Hello, World!");
+    public static void main(String[] args)
+    {
+        Person person = new Person("Domenic", 24);
+        person.saysHello();
     }
 }
